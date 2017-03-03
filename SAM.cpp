a8e78ac1ea3e8292a58276795e3fa9a33aeee416
jer22 <shijieyywd@gmail.com>
2017-03-03 19:27:22 +0900

    tarjan

--- SAM.cpp
+++ SAM.cpp
@@ -9,24 +9,26 @@ const int MAXN = 250005;
 
 	// int p, q, np, nq;
 int cnt, last;
-int tr[MAXN << 1][26], len[MAXN << 1], fa[MAXN << 1];
+int tr[MAXN << 1][26], maxlen[MAXN << 1], slink[MAXN << 1], minlen[MAXN << 1];
 
 void extend(int c) {
 	int p = last;
 	int np = last = ++cnt;
-	len[np] = len[p] + 1;
-	while (!tr[p][c] && p) tr[p][c] = np, p = fa[p];
-	if (!p) fa[np] = 1;
+	maxlen[np] = maxlen[p] + 1;
+	while (!tr[p][c] && p) tr[p][c] = np, p = slink[p];
+	if (!p) slink[np] = 0, minlen[np] = 1;
 	else {
 		int q = tr[p][c];
-		if (len[p] + 1 == len[q]) fa[np] = q;
+		if (maxlen[p] + 1 == maxlen[q]) slink[np] = q, minlen[np] = maxlen[q] + 1;
 		else {
 			int nq = ++cnt;
-			len[nq] = len[p] + 1;
+			maxlen[nq] = maxlen[p] + 1;
 			memcpy(tr[nq], tr[q], sizeof(tr[q]));
-			fa[nq] = fa[q];
-			fa[np] = fa[q] = nq;
-			while (tr[p][c] == q) tr[p][c] = nq, p = fa[p];
+			slink[nq] = slink[q];
+			slink[np] = slink[q] = nq;
+			minlen[np] = minlen[q] = maxlen[nq] + 1;
+			while (tr[p][c] == q) tr[p][c] = nq, p = slink[p];
+			minlen[nq] = maxlen[slink[nq]] + 1;
 		}
 	}
 }
@@ -42,32 +44,8 @@ int main() {
 	freopen("a.in", "r", stdin);
 	scanf("%s", a);
 	scanf("%s", b);
-	cnt = last = 1;
+	cnt = last = 0;
 	build(a);
-
-
-	int now = 1;
-	int ans = 0, temp = 0;
-	for (int i = 0; i < strlen(b); i++) {
-		int id = b[i] - 'a';
-		if (tr[now][id]) {
-			now = tr[now][id];
-			temp++;
-		} else {
-			while (now && !tr[now][id]) now = fa[now];
-			if (!now) {
-				now = 1;
-				temp = 0;
-			} else {
-				temp = len[now] + 1;
-				now = tr[now][id];
-			}
-		}
-		ans = max(ans, temp);
-	}
-	printf("%d\n", ans);
-
-
 
 	return 0;
 }
--- tarjan.cpp
+++ tarjan.cpp
@@ -1,30 +1,36 @@
-
-
-
-
-int dfs(int u) {
-	stack.push(u);
+void dfs(int u) {
+	s.push(u);
 	vis[u] = 1;
-	++cnt;
-	low[u] = dfn[u] = cnt;
-	for (int j = 0; j < lin[u].size(); j++) {
-		int v = lin[u][j];
+	dfn[u] = low[u] = ++cnt;
+	for (int i = 0; i < edges[u].size(); i++) {
+		int v = edges[u][i];
 		if (!dfn[v]) {
 			dfs(v);
 			low[u] = min(low[u], low[v]);
-		} else if (vis[v]) {
+		} else if (vis[v])
 			low[u] = min(low[u], dfn[v]);
-		}
 	}
 	if (low[u] == dfn[u]) {
-		int t;
 		num++;
 		do {
-			t = stack.top();
-			col[t] = num;
-			stack.pop();
+			t = s.top();
+			s.pop();
 			vis[t] = 0;
-		}
-		while (t != u);
+			col[t] = num;
+		} while (t != u);
 	}
+}
+
+bool tarjan() {
+	cnt = num = 0;
+	while (!s.empty()) s.pop();
+	memset(dfn, 0, sizeof(dfn));
+	memset(low, 0, sizeof(low));
+	memset(vis, 0, sizeof(vis));
+	memset(col, 0, sizeof(col));
+	for (int i = 0; i < n << 1; i++)
+		if (!dfn[i]) dfs(i);
+	for (int i = 0; i < n; i++)
+		if (col[i] == col[i + n]) return false;
+	return true;
 }
