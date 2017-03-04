69caa3fa841694122c14acf06131715f41c3e603
jer22 <shijieyywd@gmail.com>
2017-03-04 14:51:38 +0900

    sam

--- SAM.cpp
+++ SAM.cpp
@@ -9,26 +9,24 @@ const int MAXN = 250005;
 
 	// int p, q, np, nq;
 int cnt, last;
-int tr[MAXN << 1][26], maxlen[MAXN << 1], slink[MAXN << 1], minlen[MAXN << 1];
+int tr[MAXN << 1][26], maxlen[MAXN << 1], slink[MAXN << 1];
 
 void extend(int c) {
 	int p = last;
 	int np = last = ++cnt;
 	maxlen[np] = maxlen[p] + 1;
 	while (!tr[p][c] && p) tr[p][c] = np, p = slink[p];
-	if (!p) slink[np] = 1, minlen[np] = 1;
+	if (!p) slink[np] = 1;
 	else {
 		int q = tr[p][c];
-		if (maxlen[p] + 1 == maxlen[q]) slink[np] = q, minlen[np] = maxlen[q] + 1;
+		if (maxlen[p] + 1 == maxlen[q]) slink[np] = q;
 		else {
 			int nq = ++cnt;
 			maxlen[nq] = maxlen[p] + 1;
 			memcpy(tr[nq], tr[q], sizeof(tr[q]));
 			slink[nq] = slink[q];
 			slink[np] = slink[q] = nq;
-			minlen[np] = minlen[q] = maxlen[nq] + 1;
 			while (tr[p][c] == q) tr[p][c] = nq, p = slink[p];
-			minlen[nq] = maxlen[slink[nq]] + 1;
 		}
 	}
 }
