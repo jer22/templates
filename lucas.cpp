de1af30868c972596125c36498b2d0891c3a97ba
jer22 <shijieyywd@gmail.com>
2017-04-06 20:12:05 +0900

    lucas

--- lucas.cpp
+++ lucas.cpp
@@ -1,36 +1,43 @@
-long long power(long long a,long long i,long long p) {
-	if (i == 0) return 1 % p;
-	long long ans = 1;
-	long long base = a;
-	while (i) {
-		if (i & 1) (ans *= base) %= p;
-		(base *= base) %= p;
-		i >>= 1;
-	}
-	return ans;
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+const unsigned long long MOD = 1000003;
+
+unsigned long long fac[MOD + 2], inv[MOD + 2];
+void init() {
+	fac[0] = 1;
+	for (unsigned int i = 1; i < MOD; i++)
+		fac[i] = fac[i - 1] * i % MOD;
+	inv[1] = 1;
+	for (unsigned int i = 2; i < MOD; i++)
+		inv[i] = (MOD - MOD / i) * inv[MOD % i] % MOD;
+	inv[0] = 1;
+	for (unsigned int i = 1; i < MOD; i++)
+		(inv[i] *= inv[i - 1]) %= MOD;
 }
 
-long long Comb(long long a, long long b, long long p) {
-	if (a < b) return 0;
-	if (a == b) return 1;
-	if (b > a - b) b = a - b;
-	long long ans = 1, ca = 1, cb = 1;
-	for (long long i = 0; i < b; ++i) {
-		ca = (ca * (a - i)) % p;
-		cb = (cb * (b - i)) % p;
-	}
-	ans = (ca * power(cb, p - 2, p)) % p;
-	return ans;
+unsigned long long C(unsigned int n, unsigned int m) {
+	if (n < m) return 0;
+	if (n < MOD && m < MOD)
+		return fac[n] * inv[m] % MOD * inv[n - m] % MOD;
+	return C(n / MOD, m / MOD) * C(n % MOD, m % MOD) % MOD;
 }
 
-long long Lucas(int n, int m, int p) {
-	long long ans = 1;
-	while(n!=0 && m!=0) {
-		ans = (ans * Comb(n % p, m % p, p)) % p;
-		 n /= p;
-		 m /= p;
+int main() {
+	freopen("4403.in", "r", stdin);
+	int T;
+	init();
+	unsigned long long n, l, r;
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%lld %lld %lld", &n, &l, &r);
+		unsigned long long m = l - r + 1;
+		printf("%lld\n", C(n + m, m) + MOD - 1) % MOD;
 	}
-	return ans;
+	return 0;
 }
-
 
