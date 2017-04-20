e5385efc4fee66203a3eae0cb8f1a59ee53eb79e
jer22 <shijieyywd@gmail.com>
2017-04-20 18:10:17 +0900

    mobius

--- mobius.cpp
+++ mobius.cpp
@@ -1,13 +1,13 @@
 long long mob[MAXN], p[1000000], check[MAXN], tot;
 void getMob() {
 	mob[1] = 1;
-	for (int i = 2; i <= n; i++) {
+	for (int i = 2; i <= 1000000; i++) {
 		if (!check[i]) {
 			p[++tot] = i;
 			mob[i] = -1;
 		}
 		for (int j = 1; j <= tot; j++) {
-			if (i * p[j] > n) break;
+			if (i * p[j] > 1000000) break;
 			check[i * p[j]] = 1;
 			if (i % p[j] == 0) {
 				mob[i * p[j]] = 0;
